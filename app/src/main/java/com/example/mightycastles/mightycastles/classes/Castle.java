11515b667672a2e4d13ea992772b677991eb9aa6
GedSelenis <selenisgediminas5@gmail.com>
2019-05-28 13:48:49 +0300

    comentaras 3

--- app/src/main/java/com/example/mightycastles/mightycastles/classes/Castle.java
+++ app/src/main/java/com/example/mightycastles/mightycastles/classes/Castle.java
@@ -1,4 +1,27 @@
 package com.example.mightycastles.mightycastles.classes;
 
 public class Castle {
+    private int id;
+    private  String name;
+    private int castleHealth;
+    private int wallHealth;
+    private int bricks;
+    private int builders;
+    private int mana;
+    private int magic;
+    private int weapons;
+    private int blacksmith;
+
+    public Castle(int id, String name, int castleHealth, int wallHealth, int bricks, int builders, int mana, int magic, int weapons, int blacksmith) {
+        this.id = id;
+        this.name = name;
+        this.castleHealth = castleHealth;
+        this.wallHealth = wallHealth;
+        this.bricks = bricks;
+        this.builders = builders;
+        this.mana = mana;
+        this.magic = magic;
+        this.weapons = weapons;
+        this.blacksmith = blacksmith;
+    }
 }
