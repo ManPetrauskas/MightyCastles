4ec413394bad4358236082c806dcd47f93ee51d2
GedSelenis <selenisgediminas5@gmail.com>
2019-05-28 14:04:27 +0300

    castle update

--- app/src/main/java/com/example/mightycastles/mightycastles/classes/Castle.java
+++ app/src/main/java/com/example/mightycastles/mightycastles/classes/Castle.java
@@ -24,4 +24,76 @@ public class Castle {
         this.weapons = weapons;
         this.blacksmith = blacksmith;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCastleHealth() {
+        return castleHealth;
+    }
+
+    public int getWallHealth() {
+        return wallHealth;
+    }
+
+    public int getBricks() {
+        return bricks;
+    }
+
+    public int getBuilders() {
+        return builders;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getMagic() {
+        return magic;
+    }
+
+    public int getWeapons() {
+        return weapons;
+    }
+
+    public int getBlacksmith() {
+        return blacksmith;
+    }
+
+    public void setCastleHealth(int castleHealth) {
+        this.castleHealth = castleHealth;
+    }
+
+    public void setWallHealth(int wallHealth) {
+        this.wallHealth = wallHealth;
+    }
+
+    public void setBricks(int bricks) {
+        this.bricks = bricks;
+    }
+
+    public void setBuilders(int builders) {
+        this.builders = builders;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public void setMagic(int magic) {
+        this.magic = magic;
+    }
+
+    public void setWeapons(int weapons) {
+        this.weapons = weapons;
+    }
+
+    public void setBlacksmith(int blacksmith) {
+        this.blacksmith = blacksmith;
+    }
 }
