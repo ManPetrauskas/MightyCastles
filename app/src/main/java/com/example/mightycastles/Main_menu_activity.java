0ccb7767194b41445bac1986b392be858b32c497
GedSelenis <selenisgediminas5@gmail.com>
2019-05-28 18:20:35 +0300

    castle update

--- app/src/main/java/com/example/mightycastles/Main_menu_activity.java
+++ app/src/main/java/com/example/mightycastles/Main_menu_activity.java
@@ -1,13 +1,28 @@
 package com.example.mightycastles;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main_menu_activity extends AppCompatActivity {
-
+    private Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu_activity);
+        button = (Button) findViewById(R.id.playButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openPlayActivity();
+            }
+        });
+
+    }
+    public void openPlayActivity(){
+        Intent intent = new Intent(this, PlayActivity.class);
+        startActivity(intent);
     }
 }
