e36162f633804d45a32ea72d3c7375d2707c5645
GedSelenis <selenisgediminas5@gmail.com>
2019-05-28 18:08:01 +0300

    castle update

---
+++ app/src/main/java/com/example/mightycastles/Main_menu_activity.java
@@ -0,0 +1,13 @@
+package com.example.mightycastles;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main_menu_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_menu_activity);
+    }
+}
