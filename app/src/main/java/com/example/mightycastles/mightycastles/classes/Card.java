9651695007231380701b049b24ed81bdf2cf514c
ManPetrauskas <m.petrauskas98@gmail.com>
2019-05-28 13:50:02 +0300

    Updatinta Card class

--- app/src/main/java/com/example/mightycastles/mightycastles/classes/Card.java
+++ app/src/main/java/com/example/mightycastles/mightycastles/classes/Card.java
@@ -2,11 +2,11 @@ package com.example.mightycastles.mightycastles.classes;
 
 public class Card {
 
-    int id;
-    String name;
-    int mana;
-    int bricks;
-    int weapons;
+   private int id;
+   private String name;
+   private int mana;
+   private int bricks;
+   private int weapons;
 
     public Card(int id, String name, int mana, int bricks, int weapons) {
         this.id = id;
