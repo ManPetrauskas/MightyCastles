dec63eb423937767a68b8b0e7544b9ea7f22abd6
GedSelenis <selenisgediminas5@gmail.com>
2019-05-29 22:41:51 +0300

    Cards

--- app/src/main/java/com/example/mightycastles/Gameplay.java
+++ app/src/main/java/com/example/mightycastles/Gameplay.java
@@ -2,12 +2,38 @@ package com.example.mightycastles;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Gameplay extends AppCompatActivity {
-
+    private Button handButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gameplay);
+
+        handButton = (Button) findViewById(R.id.showHand);
+        handButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showCardsToggle();
+            }
+        });
+    }
+    public void showCardsToggle(){
+        if (findViewById(R.id.card1Button).getVisibility() ==View.VISIBLE){
+            findViewById(R.id.card2Button).setVisibility(View.INVISIBLE);
+            findViewById(R.id.card3Button).setVisibility(View.INVISIBLE);
+            findViewById(R.id.card4Button).setVisibility(View.INVISIBLE);
+            findViewById(R.id.card5Button).setVisibility(View.INVISIBLE);
+            findViewById(R.id.card6Button).setVisibility(View.INVISIBLE);
+        }
+        else{
+            findViewById(R.id.card2Button).setVisibility(View.VISIBLE);
+            findViewById(R.id.card3Button).setVisibility(View.VISIBLE);
+            findViewById(R.id.card4Button).setVisibility(View.VISIBLE);
+            findViewById(R.id.card5Button).setVisibility(View.VISIBLE);
+            findViewById(R.id.card6Button).setVisibility(View.VISIBLE);
+        }
     }
 }
